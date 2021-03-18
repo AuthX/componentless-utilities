664394cd43c5596d6b92b42d3221fd09a9fd29fd
lewis-thomas <lewis.thomas@beauthentic.digital>
2021-03-18 09:27:24 -0400

    Value list by channel (#7)
    * ValueListUtility updated to search value-lists path. Set to case insensitive
    
    * updated version
    
    * allowing info inheritance for value list and resource bundles
    
    * naming resource bundles field

--- src/main/java/com/authentic/util/ValueListUtility.java
+++ src/main/java/com/authentic/util/ValueListUtility.java
@@ -5,7 +5,9 @@ import com.google.common.base.Strings;
 import org.hippoecm.hst.container.RequestContextProvider;
 import org.hippoecm.hst.content.beans.standard.HippoBean;
 import org.hippoecm.hst.core.component.HstRequest;
+import org.hippoecm.hst.core.parameters.Parameter;
 import org.hippoecm.hst.core.request.HstRequestContext;
+import org.hippoecm.hst.util.HstResponseUtils;
 import org.onehippo.forge.selection.hst.contentbean.ValueList;
 import org.onehippo.forge.selection.hst.contentbean.ValueListItem;
 
@@ -20,17 +22,23 @@ public class ValueListUtility {
 
     public static void addValueListsToModel(final HstRequest request, final ValueListUtility.Info info) {
         final String ids = info.getValueLists();
+        addValueListsByIdsToModel(request, ids);
 
+    }
+
+    public static void addValueListsByIdsToModel(final HstRequest request, final String ids) {
         if (!Strings.isNullOrEmpty(ids)) {
             final Map<String, Map<String, String>> valueLists = getValueListMaps(Arrays.asList(ids.split(",")));
             valueLists.forEach(request::setModel);
         }
-    }
 
+    }
     public static ValueList getValueList(String path) {
         final HstRequestContext hstRequestContext = RequestContextProvider.get();
         final HippoBean siteContentBaseBean = hstRequestContext.getSiteContentBaseBean();
         HippoBean bean = siteContentBaseBean.getBean(path);
+        if (bean == null)
+            bean = siteContentBaseBean.getBean(String.format("value-lists/%s", path.toLowerCase()));
         if (bean == null)
             bean = siteContentBaseBean.getParentBean().getBean(String.format("administration/value-lists/%s", path));
         if (bean == null)
@@ -62,9 +70,8 @@ public class ValueListUtility {
         return maps;
     }
 
-    public interface Info extends ComponentlessInfo {
-        default String getValueLists() {
-            return getStringParameter("valueLists");
-        }
+    public interface Info {
+        @Parameter(name = "valueLists", displayName = "Value Lists")
+        String getValueLists();
     }
 }
